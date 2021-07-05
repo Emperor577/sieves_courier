e82906d86df604570e5b128d49060ff4675ab169
emperor577 <kholmuhammadov@gmail.com>
2021-07-05 15:49:11 +0500

    added home screen changed manin

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate to the second screen when tapped.
+          },
+          child: Text('Launch screen'),
+        ),
+      ),
+    );
+  }
+}
